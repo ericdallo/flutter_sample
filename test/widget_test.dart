e99e05dfca4573cdf14c9357403b1607c4102813
Eric Dallo <ericdallo06@hotmail.com>
2023-11-19 17:38:47 -0300

    Update sample project

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,8 +15,6 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    debugPrint('--------->');
-
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
